1085ee6b4a18552ef0cb720144e7ccbc2d97a06f
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-06-18 08:22:56 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# fly
 
-## Description
+## Container image with Concourse Fly and AWS CLI
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "fly" {
+  name = "fly"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/fly" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "fly" {
+  repository = aws_ecr_repository.fly.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.fly.repository_url
 }
